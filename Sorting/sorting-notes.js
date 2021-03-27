f78913be21c961903f7e5ee3bc9397939544d981
Brent Gauthier <brentsgauthier@gmail.com>
2021-03-27 09:49:16 -0400

    Assignments completed

---
+++ Sorting/sorting-notes.js
@@ -0,0 +1,92 @@
+//  Bubble Sort
+// Worst case scenario
+// In bubble sort, you keep looping through an array to find out whether adjacent values need swapping, 
+//  and you keep going until there are no more values that need swapping:
+function swap(array, i, j) {
+    const tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
+};
+
+function bubbleSort(array) {
+    let swaps = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > array[i + 1]) {
+            swap(array, i, i + 1);
+            swaps++;
+        }
+    }
+
+    if (swaps > 0) {
+        return bubbleSort(array);
+    }
+    return array;
+};
+//  Average and worst case time scenario is O(n^2)
+
+//  Merge Sort
+//  Divide and conquer. Breaks array down into smaller chunks, sorts, merges.
+function mergeSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    const middle = Math.floor(array.length / 2);
+    let left = array.slice(0, middle);
+    let right = array.slice(middle, array.length);
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+    return mergeSort(left, right, array)
+};
+
+function merge(left, right, array) {
+    let leftIndex = 0;
+    let rightIndex = 0;
+    let outputIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            array[outputIndex++] = left[leftIndex++];
+        }
+        else {
+            array[outputIndex++] = right[rightIndex++];
+        }
+    }
+
+    for (let i = leftIndex; i < left.length; i++) {
+        array[outputIndex++] = left[i];
+    }
+
+    for (let i = rightIndex; i < right.length; i++) {
+        array[outputIndex++] = right[i];
+    }
+    return array;
+}
+//  Only time scenario is O(nlog(n)). Still significantly better than O(n^2)
+
+//  Quicksort
+//  Best and Average time scenario O(nlog(n)), worst case O(n^2))
+//  More cache efficient, can easily be performed in place without additional memory allocations
+function quickSort(array, start = 0, end = array.length) {
+    if (start >= end) {
+        return array;
+    }
+    const middle = partition(array, start, end);
+    array = quickSort(array, start, middle);
+    array = quickSort(array, middle + 1, end);
+    return array;
+};
+
+//  Parting algorithm
+function partition(array, start, end){
+    const pivot = array[end - 1];
+    let j = start;
+    for (let i = start; i < end - 1; i++) {
+        if (array[i] <= pivot) {
+            swap(array, i, j);
+            j++;
+        }
+    }
+    swap(array, end-1, j);
+    return j;
+};
