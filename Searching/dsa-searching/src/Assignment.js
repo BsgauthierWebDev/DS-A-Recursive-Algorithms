4985240e0af3e2d2830bfff640af7697468ab508
Brent Gauthier <brentsgauthier@gmail.com>
2021-03-26 20:03:35 -0400

    Initial commit

---
+++ Searching/dsa-searching/src/Assignment.js
@@ -0,0 +1,10 @@
+//  1. How many searches
+//  1A. Using binary search, identify sequence of numbers to find 8
+//  Sorted List: [3, 5, 6, 8, 11, 12, 14, 15, 17, 18] 
+//  Answer: [12, 6, 8]
+//  1B. Using recursive binary search algorithm, identify sequence of numbers
+//  will search to try and find 16
+//  Sorted List: [3, 5, 6, 8, 11, 12, 14, 15, 17, 18] 
+//  Answer: [12, 17, 14, 15, -1]
+
+//  2. Adding a React UI
